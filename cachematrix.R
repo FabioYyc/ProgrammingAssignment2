6a14bd8343301a47e0b791ee2e9b9eca8122e24d
FabioYyc <45674168+FabioYyc@users.noreply.github.com>
2018-12-07 13:43:55 +1100

    Add files via upload
    submission of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions set a matrix in the parent environment then invert the matrix
 
-## Write a short comment describing this function
+## Build a set of funtions and returns the matrix into parent environment
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {inv = NULL
+
+set <- function(y){
+  x <<- y
+  inv  = NULL}
+get <- function()x
+setinverse <-function(solve)inv <<-solve
+getinverse <- function()inv
+
+list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## retrive the matrix that was setted in makeCacheMatrix function
+##and return the inverted matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  inv <-x$getinverse
+  if(!is.null(inv)){return(inv)}
+  
+  matrix <- x$get
+
+  inv <- solve(matrix, ...)
+  x$setinverse(inv)
+  inv
+  }
